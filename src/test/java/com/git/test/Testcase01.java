16dad5c0eacf69200cf5727eefee6f0c2eb15b9d
user <user@192.168.77.1>
2024-02-12 22:39:11 +0530

    tc1 updated

--- src/test/java/com/git/test/Testcase01.java
+++ src/test/java/com/git/test/Testcase01.java
@@ -1,11 +1,14 @@
 package com.git.test;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class Testcase01 {
-	
+	@Test
 	public void m1()
 	{
 		
-		
+	Reporter.log("m1 in eng2");	
 	}
 
 }
