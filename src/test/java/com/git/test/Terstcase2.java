027b6e5e762bf477d55df89541db883db57487c6
user <user@192.168.77.1>
2024-02-12 22:44:02 +0530

    updated t2

--- src/test/java/com/git/test/Terstcase2.java
+++ src/test/java/com/git/test/Terstcase2.java
@@ -1,5 +1,15 @@
 package com.git.test;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class Terstcase2 {
+	@Test
+	public void m2()
+	{
+		
+	Reporter.log("m2 in eng1");	
+	}
+
 
 }
